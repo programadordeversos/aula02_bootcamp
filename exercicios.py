a0e87288b89b701ccd0900c5fcdc060092dd48ae
Samuel dos Santos <samuelsantosp10@gmail.com>
2024-10-17 16:06:59 -0300

    Exercicíos do 01 ao 05 realizados

--- exercicios.py
+++ exercicios.py
@@ -1,15 +1,47 @@
 # #### Inteiros (`int`)
 
-# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário
+
+# numero_01_1 = int(input("Inserir um numero inteiro: "))
+# numero_02_1 = int(input("Insira outro numero inteiro: "))
+# resultado_1 = numero_01_1 ++ numero_02_1
+
+# print(f"O resultado da soma é: {resultado_1}")
+
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# numero_01_2 = int(input("Insira um numero inteiro: "))
+# divisor = 5
+# resultado_2 = numero_01_2 // divisor
+# resto_2 = float(numero_01_2 % divisor)
+
+# print(f"O resultado da divisão é {resultado_2} O resto da divisão é {resto_2}")
+
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# numero_01_3 = int(input("Insira um numero inteiro: "))
+# numero_02_3 = int(input("Insira outro numero inteiro: "))
+# resultado = numero_01_3 * numero_02_3
+
+# print(f"O resultado da multiplicação é: {resultado}")
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+# numero_01_4 = int(input("Inserir um numero inteiro: "))
+# numero_02_4 = int(input("Inserir outro numero inteiro: "))
+# resultado_4 = numero_01_4 // numero_02_4
+# print(resultado_4)
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
-numero_01 = int(input("Inserir um numero inteiro: "))
-numero_02 = int(input("Inserir outro numero inteiro: "))
-resultado = numero_01 // numero_02
-print(resultado)
+import math
+
+raio_do_circulo = float(input("Digite o raio: "))
+area_do_circulo = math.pi * raio_do_circulo ** 2
+
+print(area_do_circulo)
 
 # #### Números de Ponto Flutuante (`float`)
 
